e74e55a91db3f43a7c3efdd9e5c57591641ff0c1
tianrq <ietianrq@163.com>
2024-08-29 22:34:15 +0800

    idea first commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-publi class HelloWorld {
+public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println(“HelloWorld”);
+        System.out.println("HelloWorld");
     }
 }
